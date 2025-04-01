b0630c1c939477956217aa7f23e7d8acf117501e
madvaies <m.andrew.davies@gmail.com>
2025-04-01 13:29:23 -0400

    oAuth twitter to production v2

--- app/api/twitter/auth/route.ts
+++ app/api/twitter/auth/route.ts
@@ -3,14 +3,12 @@ import { randomBytes, createHash } from 'crypto';
 
 export async function GET() {
   const clientId = process.env.TWITTER_CLIENT_ID!;
-  const redirectUri = 'https://findyourchimps.dev/api/twitter/callback'; // Update for production
+  const redirectUri = process.env.TWITTER_REDIRECT_URI!; // Use env variable
   const state = randomBytes(16).toString('hex');
   const codeVerifier = randomBytes(32).toString('base64url');
-  const codeChallenge = createHash('sha256')
-    .update(codeVerifier)
-    .digest('base64url');
+  const codeChallenge = createHash('sha256').update(codeVerifier).digest('base64url');
 
-  const authUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=tweet.read%20users.read%20bookmark.read&state=${state}&code_challenge=${codeChallenge}&code_challenge_method=S256`;
+  const authUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=tweet.read%20users.read%20bookmark.read&state=${state}&code_challenge=${codeChallenge}&code_challenge_method=S256`;
 
   const response = NextResponse.redirect(authUrl);
   response.cookies.set('twitter_code_verifier', codeVerifier, { httpOnly: true, path: '/' });
